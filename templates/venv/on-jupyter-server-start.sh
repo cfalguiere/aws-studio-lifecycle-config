541682a58db620afcf19c8edb5e0968f90b95f2f
Claude Falguiere <cfalguiere@gmail.com>
2022-10-25 14:21:19 +0000

    added kernel update

--- templates/venv/on-jupyter-server-start.sh
+++ templates/venv/on-jupyter-server-start.sh
@@ -5,11 +5,23 @@
 
 set -eux
 
-echo "cloning $SUBST_ENV_URL in env-$SUBST_ENV_NAME"
-git clone "$SUBST_ENV_URL" "${HOME}/env-${SUBST_ENV_NAME}" || { printf '%s\n' "cloning failed" >&2; exit 1; }
+
+
+if [ ! -d "${HOME}/env-${SUBST_ENV_NAME}" ] ; then
+    echo "cloning $SUBST_ENV_URL in env-$SUBST_ENV_NAME"
+    git clone "$SUBST_ENV_URL" "${HOME}/env-${SUBST_ENV_NAME}" || { printf '%s\n' "cloning failed" >&2; exit 1; }
+else
+    echo "Updating clone in env-$SUBST_ENV_NAME"
+    git -C "${HOME}/env-${SUBST_ENV_NAME}" pull 
+fi
 
 # setup the required env
-conda env create -f "${HOME}/env-${SUBST_ENV_NAME}/venv-config/conda-env.yml" --verbose
+# WARNING expects the name given as parameter to be the name in the config file
+echo "Creating venv in $SUBST_ENV_NAME"
+conda env create -f "${HOME}/env-${SUBST_ENV_NAME}/venv-config/conda-env.yml" --force
+# --verbose 
 
-# activate the required env
-conda activate "$SUBST_ENV_NAME"
+# register as Jupyter kernel
+python -m ipykernel install --name="${SUBST_ENV_NAME}"
+# --user 
+
